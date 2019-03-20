b5c082af558623cba91192e9df71eea32cbcea8f
Petrit Salihu <petrit368@gmail.com>
2019-03-20 13:16:49 +0100

    Menu import

---
+++ Assets/Petrit/Menu/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LaadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void StopSpel()
+    {
+        Application.Quit();
+    }
+
+}
