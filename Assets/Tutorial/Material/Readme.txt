340d3ad5355958488e170960837c1a6adba2e69a
Meindert <meindert.holtappels@gmail.com>
2019-04-09 13:34:18 +0200

    tutorial

---
+++ Assets/Tutorial/Material/Readme.txt
@@ -0,0 +1,36 @@
+
+
+
+
+сссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссс
+HVD Fonts | HVD Poster (Freefont)
+сссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссссс
+
+
+
+е This typeface is Copyright 2006/2009 Hannes von DЪhren.
+
+е This typeface may not be redistributed.
+
+е This typeface may not be modified in any way.
+
+е This typeface may not be sold.
+		
+е HVD Fonts is not liable for any damage resulting from the use of this typeface.
+
+е This typeface is free for personal and commercial use.
+
+
+Have Fun!
+
+
+NOTE: HVD Poster is designed for use in big sizes (print). It has
+many vectorpoints which can slow down the performance of vectorbased
+programs (e.g. Flash). 
+Please use the HVD Poster Clean for those applications. It's number of 
+vectorpoints is reduced.
+
+
+Contact: 
+www.hvdfonts.com
+hannes@hvdfonts.com
---
+++ Assets/Tutorial/Scripts/PlayerController.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+    private PlayerMotor motor;
+
+    public GameObject ObjectText;
+    public GameObject Object1Text;
+    public GameObject Object2Text;
+    public GameObject DoorClosedText;
+    public GameObject DoorOpenText;
+    public GameObject Door;
+
+    bool textActive;
+    bool Doorclosed;
+    
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+        textActive = false;
+        ObjectText.SetActive(false);
+        Object1Text.SetActive(false);
+        Object2Text.SetActive(false);
+        DoorClosedText.SetActive(false);
+        DoorOpenText.SetActive(false);
+        Doorclosed = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        motor.Move(_velocity);
+
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        motor.Rotate(_rotation);
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        motor.RotateCamera(_cameraRotation);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Object")
+        {
+            ObjectText.SetActive(true);
+            textActive = true;
+            Debug.Log("gang gang");
+        }
+
+        if (other.gameObject.tag == "Object1")
+        {
+            Object1Text.SetActive(true);
+            textActive = true;
+            Debug.Log("gang gang");
+        }
+
+        if (other.gameObject.tag == "Object2")
+        {
+            Object2Text.SetActive(true);
+            textActive = true;
+            Debug.Log("gang gang");
+        }       
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Door")
+        {
+            DoorClosedText.SetActive(true);
+            textActive = true;
+            //Debug.Log("Sjaan blijf van me deur af");
+            Dooropen();
+            DoorClosed();
+        }
+    }
+
+    void Dooropen()
+    {
+        if (Doorclosed == true)
+        {
+            DoorClosedText.SetActive(true);
+            DoorOpenText.SetActive(false);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Door.transform.position = new Vector3(-0.368f, 0f, 3.67f);
+                Door.transform.eulerAngles = new Vector3(0f, 90f, 0f);
+                Debug.Log("Yeet");
+                DoorClosed();
+                Doorclosed = false;
+            }
+        }
+    }
+
+    void DoorClosed()
+    {
+        if(Doorclosed == false)
+        {
+            DoorClosedText.SetActive(false);
+            DoorOpenText.SetActive(true);
+
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                Door.transform.position = new Vector3(-0.018f, 0f, 3.279f);
+                Door.transform.eulerAngles = new Vector3(0f, 180f, 0f);
+                Debug.Log("Yordi");
+                Doorclosed = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+        ObjectText.SetActive(false);
+        Object1Text.SetActive(false);
+        Object2Text.SetActive(false);
+        DoorClosedText.SetActive(false);
+        DoorOpenText.SetActive(false);
+        textActive = false;
+    }
+}
